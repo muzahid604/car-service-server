6c64c91d59ad65d3205929b3a76e0e06a1f67897
muzahid604 <604muzahid@gmail.com>
2022-04-28 12:21:57 +0600

    service data loaded

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//user:car-service1
+//pass:JMV6FSzw1kfIKpAf
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdqpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+
+    try {
+        await client.connect();
+
+        const serviceCollection = client.db('car-service1').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('car service is worked')
+})
+
+app.listen(port, () => {
+    console.log('server is run')
+})
